1b49daca7bc17682f0f1042f07e39a9ac319e19a
Sandree <antang.sandree.lebico@gmail.com>
2026-05-17 18:56:37 +0800

    3 hrs in the making

--- week1/day2/script.js
+++ week1/day2/script.js
@@ -1,142 +1,214 @@
-// Day 2: Exercises 
-
-// Exercise level 1  
-
 // 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 
-let challenge = '30 Days of Javascript'
+let challenge = '30 Days Of JavaScript';
 
 // 2. Print the string on the browser console using console.log()
 
-console.log(challenge) // 30 Days of Javascript
+console.log(challenge);
 
 // 3. Print the length of the string on the browser console using console.log()
 
-console.log(challenge.length) // 21
+console.log(challenge.length);
 
 // 4. Change all the string characters to capital letters using toUpperCase() method
 
-console.log(challenge.toUpperCase()) // 30 DAYS OF JAVASCRIPT
+console.log(challenge.toUpperCase());
 
 // 5. Change all the string characters to lowercase letters using toLowerCase() method
 
-console.log(challenge.toLowerCase()) // 30 days of javascript
+console.log(challenge.toLowerCase());
 
 // 6. Cut (slice) out the first word of the string using substr() or substring() method
 
-console.log(challenge.substr(0,3)) // 30
-console.log(challenge.substring(0,3)) // 30
+console.log(challenge.substring(0,2));
 
 // 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
 
-console.log(challenge.substring(3)) // Days of Javascript
+console.log(challenge.substring(3,20));
 
-//8. Check if the string contains a word Script using includes() method
+// 8. Check if the string contains a word Script using includes() method
 
-console.log(challenge.includes('script')) // true
+console.log(challenge.includes('Script'));
 
-//9. Split the string into an array using split() method
+// 9. Split the string into an array using split() method
 
-console.log(challenge.split(''))  // [ '3', '0', ' ', 'D', 'a', 'y', 's', ' ', 'o', 'f', ' ', 'J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p',  't']
+console.log(challenge.split());
 
 // 10. Split the string 30 Days Of JavaScript at the space using split() method
 
-console.log(challenge.split(' ')) // [ '30', 'Days', 'of', 'Javascript' ]
+console.log(challenge.split(' '));
 
 // 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 
-let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
-console.log(string.split(', ' )) // [  'Facebook',  'Google',  'Microsoft',  'Apple',  'IBM',  'Oracle',  'Amazon']
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+console.log(companies.split(','));
 
 // 12. Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
 
-console.log(challenge.replace('Javascript', 'Phython')) // 30 Days of Phython
+console.log(challenge.replace('JavaScript', 'Python'));
 
 // 13. What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
 
-console.log(challenge.charAt(15)) //s
+console.log(challenge.charAt(15));
 
 // 14. What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
 
-console.log(challenge.charCodeAt('J')) //51
+console.log(challenge.charCodeAt('J'));
 
-// 15, Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+// 15. Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
 
-console.log(challenge.indexOf()) //-1
+console.log(challenge.indexOf('30'));
 
 // 16. Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
 
-console.log(challenge.lastIndexOf()) //-1
+console.log(challenge.lastIndexOf('JavaScript'));
 
-// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// 17. Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
-let sentence = 'You cannot end a sentence with because because because is a conjunction'
-console.log(sentence.indexOf('because')) //31
+console.log('You cannot end a sentence with because because because is a conjunction'.indexOf('cannot'));
 
-// Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// 18. Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
-console.log(sentence.lastIndexOf('because')) //47
+console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf('conjunction'));
 
-// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+//19. Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
-console.log(sentence.search('because')) //31
+console.log('You cannot end a sentence with because because because is a conjunction'.search('You'));
 
-// Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+// 20. Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
 
-let text = ' 30 Days Of JavaScript '
-console.log(text) //  30 Days Of JavaScript 
-console.log(text.trim()) //30 Days Of JavaScript
+let challenge2 = ' 30 Days of JavaScript '
 
-// Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge2.trim(' '))
 
-console.log(challenge.startsWith(30)) //true
+// 21. Use startsWith() method with the string 30 Days Of JavaScript and make the result true
 
-// Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.startsWith('30'));
 
-console.log(challenge.endsWith("Javascript")) //true
+//22. Use endsWith() method with the string 30 Days Of JavaScript and make the result true
 
-// Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(challenge.endsWith('JavaScript'));
 
-console.log(challenge.match('a')) // [ 'a', index: 4, input: '30 Days of Javascript', groups: undefined ]
+//23. Use match() method to find all the a’s in 30 Days Of JavaScript
 
-// Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+console.log(challenge.match('a'));
 
-let text1 = '30 days of'
-let text2 = 'Javascript'
-console.log(text1.concat(text2)) // 30 days ofJavascript
+//24. Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 
-// Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log('30 Days of'.concat('JavaScript'));
 
-console.log(challenge.repeat(2)) // 30 Days of Javascript30 Days of Javascript
+// 25. Use repeat() method to print 30 Days Of JavaScript 2 times
 
-// Exercise Level 2
-
-//Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
-
-let quote = 'There is no exercise better for the heart than reaching down and lifting people up.'
-
-console.log(`The quote ${quote} by John Holmes teaches us to help one another.`)
-
-// Using console.log() print out the following quote by Mother Teresa:
-
-let author = "-- Mother Teresa"
-
-console.log(`Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead. ${author}` )
-
-// Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
-
-let number = 10 
-let num = Number(number)
-
-console.log(typeof 10 == typeof num) //true 
-
-// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-
-let float = parseFloat('9.8')
-
-console.log(Math.round(float)) //10
+console.log(challenge.repeat(2));
 
 
+// Exercise Level 2 
+
+
+// 1. Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+
+// 2. Using console.log() print out the following quote by Mother Teresa: "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+
+// 3. Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+let number = 10;
+
+console.log(typeof 10);
+
+// 4. Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+
+let float = '9.8'
+let num = parseFloat(float)
+
+console.log(Math.round(num));
+
+// 5. Check if 'on' is found in both python and jargon
+
+console.log('python'.includes('on'),'jargon'.includes('on'));
+
+// 6. I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+console.log('I hope this course is not full of jargon'.includes('jargon'));
+
+// 7. Generate a random number between 0 and 100 inclusively.
+
+let randomnum = Math.random()*100
+let roundednum = Math.round(randomnum)
+console.log(roundednum);
+
+//8. Generate a random number between 50 and 100 inclusively.
+
+let fifty = Math.random() * (100 - 50+1) + 50;
+
+console.log(fifty)
+
+//9. Generate a random number between 0 and 255 inclusively.
+
+let twofiftyfayb = Math.random() * 256;
+
+console.log(twofiftyfayb)
+
+
+// 10. Access the 'JavaScript' string characters using a random number.
+
+const str = 'JavaScript';
+const randomIndex = Math.floor(Math.random() * str.length);
+const randomChar = str[randomIndex];
+
+console.log(`Random Character at index ${randomIndex}: ${randomChar}`);
+
+// 11. Use console.log() and escape characters to print the following pattern.
+
+
+console.log((String(Math.floor(Math.random() * 100)).repeat(5) + "\n").repeat(5))
+
+
+// Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+
+let sentence = 'You cannot end a sentence with because because because  is a conjunction';
+
+console.log(sentence.substring(0,38) , sentence.substring(55,75))
+
+console.log(sentence.substring(31,54))
+
+// Exercise 3 
+
+// 1. 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+let sentence2 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let pattern = /love/gi
+console.log(sentence2.match(pattern).length)
+
+
+// 2. Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let pattern2 = /because/gi 
+
+console.log(sentence.match(pattern2).length);
+
+// 3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+const sentence3 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
 
 
+console.log(sentence3.replace(/[%$&#@;]/gi, ''));
+
+//4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+const problem = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let pattern3 = /\d+/g;
+let match = problem.match(pattern3)
+let salarypermonth = parseInt(match[0])
+let annualbonus = parseInt(match[1])
+let coursespermonth = parseInt(match[2])
+let totalAnnualSalary = parseInt((salarypermonth * 12) + (coursespermonth *12) + annualbonus);
+
+console.log(`He earns total annual salary of ${totalAnnualSalary}`);
